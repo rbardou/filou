e5edde576297d981fc3f397703b39fe63364f86d
Romain <>
2021-08-25 09:45:05 +0200

    add type at the beginning of config files

--- config.ml
+++ config.ml
@@ -9,14 +9,19 @@ type t =
   | Main
   | Clone of clone
 
-let write buffer = function
-  | Main ->
-      W.int_u8 buffer 0
-  | Clone { main_location } ->
-      W.int_u8 buffer 1;
-      W.(string int_u16) buffer (Device.show_location main_location)
+let write buffer config =
+  W.(string @@ fun _ _ -> ()) buffer "Fconfig1";
+  match config with
+    | Main ->
+        W.int_u8 buffer 0
+    | Clone { main_location } ->
+        W.int_u8 buffer 1;
+        W.(string int_u16) buffer (Device.show_location main_location)
 
 let read buffer =
+  let typ = R.(string @@ fun _ -> 8) buffer in
+  if typ <> "Fconfig1" then
+    raise (Failed [ sf "unknown config file type: %S" typ ]);
   (* TODO: expect eof? *)
   match R.int_u8 buffer with
     | 0 ->
